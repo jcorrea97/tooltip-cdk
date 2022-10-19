46554a85aa5765b67d835b2b31992a10a467d0ed
jcorrea97 <jroberto.cr1@gmail.com>
2022-10-19 12:46:23 -0300

    cria input para posicao da tooltip

--- src/app/tooltip/tooltip-overlay/tooltip.directive.ts
+++ src/app/tooltip/tooltip-overlay/tooltip.directive.ts
@@ -1,19 +1,38 @@
-import { ComponentRef, Directive, ElementRef, HostListener, Input } from '@angular/core';
+import {
+  ComponentRef,
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+} from '@angular/core';
 import {
   OverlayRef,
   Overlay,
   OverlayPositionBuilder,
   ConnectionPositionPair,
+  OriginConnectionPosition,
+  OverlayConnectionPosition,
 } from '@angular/cdk/overlay';
 import { TooltipOverlayComponent } from './tooltip-overlay.component';
 import { ComponentPortal } from '@angular/cdk/portal';
+
+export const POSITIONS = ['top', 'right', 'bottom', 'left'];
 
 @Directive({
   selector: '[appTooltip]',
 })
 export class TooltipDirective {
+  private _tooltipPosition: string = 'bottom';
 
   @Input('appTooltip') text = '';
+
+  @Input('appTooltip-position') set tooltipPosition(position: string) {
+    this._tooltipPosition = POSITIONS.includes(position) ? position : 'bottom';
+  }
+
+  get tooltipPosition(): string {
+    return this._tooltipPosition;
+  }
 
   private overlayRef: OverlayRef;
   private tooltipRef: ComponentRef<TooltipOverlayComponent>;
@@ -25,48 +44,78 @@ export class TooltipDirective {
   ) {}
 
   createTooltip() {
-    const positionStrategy = this.overlayPositionBuilder.flexibleConnectedTo(this.elementRef).withPositions([
-      {
-        originX: 'center',
-        originY: 'bottom',
-        overlayX: 'center',
-        overlayY: 'top',
-        offsetY: 5,
-      },
-      //caso não tenha espaço na sua tooltip o cdk vai tentar as posições do ConnectionPositionPair
-      //bottom
-      new ConnectionPositionPair(
-        { originX: 'center', originY: 'bottom' },
-        { overlayX: 'center', overlayY: 'top' },
-        0,
-        10,
-      ),
-      //top
-      new ConnectionPositionPair(
-        { originX: 'center', originY: 'top' },
-        { overlayX: 'center', overlayY: 'bottom' },
-        0,
-        -10
-      ),
-      //left
-      new ConnectionPositionPair(
-        { originX: 'start', originY: 'center' },
-        { overlayX: 'end', overlayY: 'center' },
-        -10,
-        0,
-      ),
-      //right
-      new ConnectionPositionPair(
-        { originX: 'end', originY: 'center' },
-        { overlayX: 'start', overlayY: 'center' },
-        10,
-        0,
-      ),
-    ])
+    const chosenPosition = this.setPositon();
 
-    this.overlayRef = this.overlay.create({positionStrategy});
+    const positionStrategy = this.overlayPositionBuilder
+      .flexibleConnectedTo(this.elementRef)
+      .withPositions([
+        chosenPosition,
+        //caso não tenha espaço na sua tooltip o cdk vai tentar as posições do ConnectionPositionPair
+        //bottom
+        new ConnectionPositionPair(
+          { originX: 'center', originY: 'bottom' },
+          { overlayX: 'center', overlayY: 'top' },
+          0,
+          10
+        ),
+        //top
+        new ConnectionPositionPair(
+          { originX: 'center', originY: 'top' },
+          { overlayX: 'center', overlayY: 'bottom' },
+          0,
+          -10
+        ),
+        //left
+        new ConnectionPositionPair(
+          { originX: 'start', originY: 'center' },
+          { overlayX: 'end', overlayY: 'center' },
+          -10,
+          0
+        ),
+        //right
+        new ConnectionPositionPair(
+          { originX: 'end', originY: 'center' },
+          { overlayX: 'start', overlayY: 'center' },
+          10,
+          0
+        ),
+      ]);
 
-    this.tooltipRef = this.overlayRef.attach(new ComponentPortal(TooltipOverlayComponent))
+    this.overlayRef = this.overlay.create({ positionStrategy });
+
+    this.tooltipRef = this.overlayRef.attach(
+      new ComponentPortal(TooltipOverlayComponent)
+    );
+  }
+
+  setPositon() {
+    let originPosition!: OriginConnectionPosition;
+    let overlayPosition!: OverlayConnectionPosition;
+    let offSet = {};
+
+    if (!this.tooltipPosition || this.tooltipPosition === 'bottom') {
+      originPosition = { originX: 'center', originY: 'bottom' };
+      overlayPosition = { overlayX: 'center', overlayY: 'top' };
+      offSet = { offsetY: 5 };      
+    } else if (this.tooltipPosition === 'top') {
+      originPosition = { originX: 'center', originY: 'top' };
+      overlayPosition = { overlayX: 'center', overlayY: 'bottom' };
+      offSet = { offsetY: -5 };
+    } else if (this.tooltipPosition === 'left') {
+      originPosition = { originX: 'start', originY: 'center' };
+      overlayPosition = { overlayX: 'end', overlayY: 'center' };
+      offSet = { offsetX: -5 };
+    } else if (this.tooltipPosition === 'right') {
+      originPosition = { originX: 'end', originY: 'center' };
+      overlayPosition = { overlayX: 'start', overlayY: 'center' };
+      offSet = { offsetX: 5 };
+    }
+
+    return {
+      ...originPosition,
+      ...overlayPosition,
+      ...offSet,
+    }
   }
 
   @HostListener('mouseenter')
@@ -75,98 +124,97 @@ export class TooltipDirective {
     this.tooltipRef.instance.text = this.text;
   }
 
-@HostListener('mouseleave')
+  @HostListener('mouseleave')
   hide() {
-    if(this.tooltipRef) {
+    if (this.tooltipRef) {
       this.tooltipRef.destroy();
       this.overlayRef.detach();
     }
   }
-
 }
 
-//todas as posições pra sua tooltip 
+//todas as posições pra sua tooltip
 
-  //BOTTOM
-  // originX: 'center',
-  // originY: 'bottom',
-  // overlayX: 'center',
-  // overlayY: 'top',
-  // offsetY: 10,
+//BOTTOM
+// originX: 'center',
+// originY: 'bottom',
+// overlayX: 'center',
+// overlayY: 'top',
+// offsetY: 10,
 
-  //bottom-left
-  // originX: 'center',
-  // originY: 'bottom',
-  // overlayX: 'end',
-  // overlayY: 'top',
-  // offsetY: 10,
+//bottom-left
+// originX: 'center',
+// originY: 'bottom',
+// overlayX: 'end',
+// overlayY: 'top',
+// offsetY: 10,
 
-  //bottom-rigth
-  // originX: 'center',
-  // originY: 'bottom',
-  // overlayX: 'start',
-  // overlayY: 'top',
-  // offsetY: 10,
+//bottom-rigth
+// originX: 'center',
+// originY: 'bottom',
+// overlayX: 'start',
+// overlayY: 'top',
+// offsetY: 10,
 
-  //TOP
-  // originX: 'center',
-  // originY: 'top',
-  // overlayX: 'center',
-  // overlayY: 'bottom',
-  // offsetY: -10,
+//TOP
+// originX: 'center',
+// originY: 'top',
+// overlayX: 'center',
+// overlayY: 'bottom',
+// offsetY: -10,
 
-  //top-left
-  // originX: 'center',
-  // originY: 'top',
-  // overlayX: 'end',
-  // overlayY: 'bottom',
-  // offsetY: -10,
+//top-left
+// originX: 'center',
+// originY: 'top',
+// overlayX: 'end',
+// overlayY: 'bottom',
+// offsetY: -10,
 
-  //top-rigth
-  // originX: 'center',
-  // originY: 'top',
-  // overlayX: 'start',
-  // overlayY: 'bottom',
-  // offsetY: -10,
+//top-rigth
+// originX: 'center',
+// originY: 'top',
+// overlayX: 'start',
+// overlayY: 'bottom',
+// offsetY: -10,
 
-  //LEFT
-  // originX: 'start',
-  // originY: 'center',
-  // overlayX: 'end',
-  // overlayY: 'center',
-  // offsetX: -10,
+//LEFT
+// originX: 'start',
+// originY: 'center',
+// overlayX: 'end',
+// overlayY: 'center',
+// offsetX: -10,
 
-  //left-bottom
-  // originX: 'start',
-  // originY: 'center',
-  // overlayX: 'end',
-  // overlayY: 'top',
-  // offsetX: -10,
+//left-bottom
+// originX: 'start',
+// originY: 'center',
+// overlayX: 'end',
+// overlayY: 'top',
+// offsetX: -10,
 
-  //left-top
-  // originX: 'start',
-  // originY: 'center',
-  // overlayX: 'end',
-  // overlayY: 'bottom',
-  // offsetX: -10,
+//left-top
+// originX: 'start',
+// originY: 'center',
+// overlayX: 'end',
+// overlayY: 'bottom',
+// offsetX: -10,
 
-  //RIGHT
-  // originX: 'end',
-  // originY: 'center',
-  // overlayX: 'start',
-  // overlayY: 'center',
-  // offsetX: 10
+//RIGHT
+// originX: 'end',
+// originY: 'center',
+// overlayX: 'start',
+// overlayY: 'center',
+// offsetX: 10
 
-  //right-top
-  // originX: 'end',
-  // originY: 'center',
-  // overlayX: 'start',
-  // overlayY: 'bottom',
-  // offsetX: 10,
+//right-top
+// originX: 'end',
+// originY: 'center',
+// overlayX: 'start',
+// overlayY: 'bottom',
+// offsetX: 10,
 
-  //right-bottom
-  // originX: 'end',
-  // originY: 'center',
-  // overlayX: 'start',
-  // overlayY: 'bottom',
-  // offsetX: 10,
+//right-bottom
+// originX: 'end',
+// originY: 'center',
+// overlayX: 'start',
+// overlayY: 'bottom',
+// offsetX: 10,
